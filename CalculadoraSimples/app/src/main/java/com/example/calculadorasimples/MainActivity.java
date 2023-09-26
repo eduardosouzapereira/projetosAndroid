edad4f008512f2dc777beaea17e1faef04be896a
Eduardo Souza Pereira <eduardosouzapereira70@gmail.com>
2023-09-26 20:42:16 -0300

    Fim da aula 4

--- CalculadoraSimples/app/src/main/java/com/example/calculadorasimples/MainActivity.java
+++ CalculadoraSimples/app/src/main/java/com/example/calculadorasimples/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.calculadorasimples;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,7 +11,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button buttonSoma;
+    Button buttonSoma, buttonSub, buttonMult, buttonDiv;
     EditText editTextValor1, editTextValor2;
 
     @Override
@@ -20,17 +21,71 @@ public class MainActivity extends AppCompatActivity {
 
         //associa objetos
         buttonSoma = findViewById(R.id.button_soma);
+        buttonSub = findViewById(R.id.button_sub);
+        buttonMult = findViewById(R.id.button_mult);
+        buttonDiv = findViewById(R.id.button_div);
         editTextValor1 = findViewById(R.id.edit_valor1);
         editTextValor2 = findViewById(R.id.edit_valor2);
 
         buttonSoma.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                double v1 = Double.parseDouble(editTextValor1.getText().toString());
-                double v2 = Double.parseDouble(editTextValor2.getText().toString());
-                double soma = v1 + v2;
-                Toast.makeText(MainActivity.this, "Resultado = " + soma, Toast.LENGTH_LONG).show();
+                mostraResultado(1);
             }
         });
+
+        buttonSub.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mostraResultado(2);
+            }
+        });
+
+        buttonMult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mostraResultado(3);
+            }
+        });
+
+        buttonDiv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mostraResultado(4);
+            }
+        });
+
+    }
+
+    public void mostraResultado(int op){
+        double resul = 0;
+
+        if (editTextValor1.getText().length() > 0 && editTextValor2.getText().length() > 0){
+            double v1 = Double.parseDouble(editTextValor1.getText().toString());
+            double v2 = Double.parseDouble(editTextValor2.getText().toString());
+            switch (op){
+                case 1:
+                    resul = v1 + v2;
+                    break;
+                case 2:
+                    resul = v1 - v2;
+                    break;
+                case 3:
+                    resul = v1 * v2;
+                    break;
+                case 4:
+                    resul = v1 / v2;
+                    break;
+                default:
+                    break;
+            }
+            AlertDialog.Builder msg = new AlertDialog.Builder(MainActivity.this);
+            msg.setTitle("Resultado");
+            msg.setNeutralButton("OK", null);
+            msg.setMessage("" + resul).show();
+            //Toast.makeText(MainActivity.this, "Resultado = " + resul, Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(MainActivity.this, "Preencha os valores!", Toast.LENGTH_SHORT).show();
+        }
     }
 }

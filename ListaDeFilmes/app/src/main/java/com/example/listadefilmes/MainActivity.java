f66c6bd002f7f5e7319c851337ae9f3975d8ebc0
Eduardo Souza Pereira <eduardosouzapereira70@gmail.com>
2023-09-28 20:52:55 -0300

    Fim da aula 9

--- ListaDeFilmes/app/src/main/java/com/example/listadefilmes/MainActivity.java
+++ ListaDeFilmes/app/src/main/java/com/example/listadefilmes/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.listadefilmes;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -16,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     EditText editTextNomeFilme;
     ListView listViewFilme;
     ArrayList<String> arrayListFilmes = new ArrayList<>();
+    TextView textViewMensagem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,18 +30,41 @@ public class MainActivity extends AppCompatActivity {
         buttonAdicionar = findViewById(R.id.buttonAdicionar);
         editTextNomeFilme = findViewById(R.id.editTextNomeFilme);
         listViewFilme = findViewById(R.id.listViewFilme);
+        textViewMensagem = findViewById(R.id.textViewMensagem);
 
         buttonAdicionar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                arrayListFilmes.add(editTextNomeFilme.getText().toString());
-                //Definir o adapter do listView
-                ArrayAdapter adapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, arrayListFilmes);
+                if(!editTextNomeFilme.getText().toString().equals("")){
+                    arrayListFilmes.add(editTextNomeFilme.getText().toString());
+                    atualizarListView();
+                    editTextNomeFilme.setText("");
+                    textViewMensagem.setVisibility(View.INVISIBLE);
+                }
 
-                //Associa adapter ao ListView
-                listViewFilme.setAdapter(adapter);
+            }
+        });
 
-                editTextNomeFilme.setText("");
+        listViewFilme.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, final int posicao, long l) {
+                AlertDialog.Builder msg = new AlertDialog.Builder(MainActivity.this);
+                msg.setMessage("Deseja realmente excluir?");
+                msg.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //Remover um item do ArrayList
+                        arrayListFilmes.remove(posicao);
+                        atualizarListView();
+
+                        if(arrayListFilmes.isEmpty()){
+                            textViewMensagem.setVisibility(View.VISIBLE);
+                        }
+                    }
+                });
+                msg.setNegativeButton("Não", null);
+                msg.show();
+                return false;
             }
         });
 
@@ -47,5 +75,14 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+    }
+
+    private void atualizarListView() {
+
+        //Definir o adapter do listView
+        ArrayAdapter adapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, arrayListFilmes);
+
+        //Associa adapter ao ListView
+        listViewFilme.setAdapter(adapter);
     }
 }
